aa68b421a9db610d5cae9f16f56912ecf26e2098
Hassan Afzaal <83668567+hafzaal@users.noreply.github.com>
2022-09-05 17:49:42 -0600

    Setup 5 rounds of rps to play per game

--- scripts/rps.js
+++ scripts/rps.js
@@ -3,16 +3,29 @@ play();
 
 function play()
 {
-    let myTurn = "Rock";
-    let cpuTurn = "Scissors";
+    for (let turnNumber = 0 ; turnNumber < 5; turnNumber++) 
+    {
+        let playerTurn = getUserTurn();
+        let cpuTurn = getCpuTurn();
+        let winner = turnOutcome(playerTurn, cpuTurn);
 
-    console.log(turnOutcome(myTurn, cpuTurn));
+        console.log(`You picked ${playerTurn}, CPU picked ${cpuTurn}\n`)
+
+        if (winner == "Draw")
+        {
+            console.log("Its a draw! Better luck next time!");
+        } else
+        {
+            console.log(`${winner} wins!!`);
+        }
+        
+    } 
 }
 
 function getUserTurn()
 {
     let userInput = prompt("Rock, Paper or Scissors?");
-    convertLowerCase(userInput);
+    userInput = convertLowerCase(userInput);
 
     return convertFirstLetterToUpperCase(userInput);
 }
@@ -20,7 +33,7 @@ function getUserTurn()
 function getCpuTurn()
 {
     const turnOptions = ["rock", "paper", "scissors"]
-    let randomChoice = Math.random() * 3;
+    let randomChoice = Math.floor(Math.random() * 3);
 
     let cpuTurn = turnOptions[randomChoice];
     
